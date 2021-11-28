a6d27746c112c08551669809722884f861389db9
mordmelena <e.mordmillovich@gmail.com>
2021-11-28 20:23:07 +0100

    Added custom font to MainMenuScreen

--- core/src/com/undercooked/game/MainMenuScreen.java
+++ core/src/com/undercooked/game/MainMenuScreen.java
@@ -2,30 +2,51 @@ package com.undercooked.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.GL20;
 
 public class MainMenuScreen implements Screen {
 
     final Undercooked game;
     OrthographicCamera camera;
+    private FreeTypeFontGenerator fontGenerator;
+    private FreeTypeFontGenerator.FreeTypeFontParameter fontParameter;
+    private BitmapFont font;
 
     public MainMenuScreen(final Undercooked gam) {
         game = gam;
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
+
+        fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("AgentOrange.ttf"));
+        fontParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        fontParameter.size = 35;
+        fontParameter.borderWidth = 2;
+        fontParameter.borderColor = Color.FIREBRICK;
+        fontParameter.color = Color.WHITE;
+        font = fontGenerator.generateFont(fontParameter);
+
+//
     }
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(0, 0, 0.2f, 1);
+
+        Gdx.gl.glClearColor(54/255f, 84/255f, 120/255f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//        ScreenUtils.clear(5, 0.7, 0.9, 1);
 
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
-        game.font.draw(game.batch, "Welcome to Drop!!! ", 100, 150);
+        font.draw(game.batch, "Undercooked!", 200, 300);
+//        game.font.draw(game.batch, "Welcome to Drop!!! ", 100, 150);
         game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
         game.batch.end();
 
